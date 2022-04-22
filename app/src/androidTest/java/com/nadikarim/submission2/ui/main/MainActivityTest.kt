f0cbd4b8abe777c284b65c75d9d130af4dc67826
nadikarim <fernadikarimhsb@gmail.com>
2022-04-22 23:34:59 +0700

    update 6

---
+++ app/src/androidTest/java/com/nadikarim/submission2/ui/main/MainActivityTest.kt
@@ -0,0 +1,75 @@
+package com.nadikarim.submission2.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.nadikarim.submission2.JsonConverter
+import com.nadikarim.submission2.R
+import com.nadikarim.submission2.utils.BASE_URL
+import com.nadikarim.submission2.utils.EspressoIdlingResource
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@MediumTest
+class MainActivityTest {
+
+    @get:Rule
+    val rule = ActivityScenarioRule(MainActivity::class.java)
+
+    private val mockWebServer = MockWebServer()
+
+
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java)
+        mockWebServer.start(8080)
+        BASE_URL = "http://127.0.0.1:8080/"
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+    }
+    @After
+    fun tearDown() {
+        mockWebServer.shutdown()
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+    }
+
+    @Test
+    fun getListStory_Success() {
+        val mockResponse = MockResponse()
+            .setResponseCode(200)
+            .setBody(JsonConverter.readStringFromFile("response.json"))
+        mockWebServer.enqueue(mockResponse)
+
+        onView(withId(R.id.rv_story)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.rv_story)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(5))
+
+    }
+
+    @Test
+    fun getListStory_ToMapView() {
+        val mockResponse = MockResponse()
+            .setResponseCode(200)
+            .setBody(JsonConverter.readStringFromFile("response.json"))
+        mockWebServer.enqueue(mockResponse)
+
+        onView(withId(R.id.menu_maps)).check(matches(isDisplayed()))
+        onView(withId(R.id.menu_maps)).perform(click())
+
+    }
+
+}
--- app/src/main/java/com/nadikarim/submission2/data/StoryRepository.kt
+++ app/src/main/java/com/nadikarim/submission2/data/StoryRepository.kt
@@ -70,22 +70,25 @@ class StoryRepository @Inject constructor(
 
 
     fun getStoryWithLocation(token: String) {
-        apiService.getListStoryWithLocation(token, 1)
-            .enqueue(object : Callback<StoriesResponse>{
-                override fun onResponse(
-                    call: Call<StoriesResponse>,
-                    response: Response<StoriesResponse>
-                ) {
-                    if (response.isSuccessful) {
-                        _listStory.postValue(response.body()?.listStory)
+        wrapEspressoIdlingResource {
+            apiService.getListStoryWithLocation(token, 1)
+                .enqueue(object : Callback<StoriesResponse>{
+                    override fun onResponse(
+                        call: Call<StoriesResponse>,
+                        response: Response<StoriesResponse>
+                    ) {
+                        if (response.isSuccessful) {
+                            _listStory.postValue(response.body()?.listStory)
+                        }
                     }
-                }
 
-                override fun onFailure(call: Call<StoriesResponse>, t: Throwable) {
-                    Log.d(RETROFIT_TAG, t.message.toString())
-                }
+                    override fun onFailure(call: Call<StoriesResponse>, t: Throwable) {
+                        Log.d(RETROFIT_TAG, t.message.toString())
+                    }
 
-            })
+                })
+        }
+
     }
 
     /*
@@ -162,33 +165,36 @@ class StoryRepository @Inject constructor(
 
 
     fun loginUser(email: String, password: String) {
-        _isLoading.value = true
-        apiService.loginUser(email, password)
-            .enqueue(object : Callback<LoginResponse> {
-                override fun onResponse(
-                    call: Call<LoginResponse>,
-                    response: Response<LoginResponse>
-                ) {
-                    _isLoading.value = false
-                    if (response.isSuccessful) {
-                        _toastMessage.value = response.body()?.message
-                        _userLogin.value = response.body()?.loginResult
-                        Log.d(RETROFIT_TAG, response.body()?.message.toString())
-                        Log.d(RETROFIT_TAG, response.body()?.loginResult?.token.toString())
-                        Log.d(RETROFIT_TAG, response.body()?.loginResult?.name ?: "name")
-                        Log.d(RETROFIT_TAG, response.body()?.loginResult?.userId ?: "userId")
+        wrapEspressoIdlingResource {
+            _isLoading.value = true
+            apiService.loginUser(email, password)
+                .enqueue(object : Callback<LoginResponse> {
+                    override fun onResponse(
+                        call: Call<LoginResponse>,
+                        response: Response<LoginResponse>
+                    ) {
+                        _isLoading.value = false
+                        if (response.isSuccessful) {
+                            _toastMessage.value = response.body()?.message
+                            _userLogin.value = response.body()?.loginResult
+                            Log.d(RETROFIT_TAG, response.body()?.message.toString())
+                            Log.d(RETROFIT_TAG, response.body()?.loginResult?.token.toString())
+                            Log.d(RETROFIT_TAG, response.body()?.loginResult?.name ?: "name")
+                            Log.d(RETROFIT_TAG, response.body()?.loginResult?.userId ?: "userId")
+                        }
+                        if (!response.isSuccessful) {
+                            _toastMessage.value = response.message()
+                        }
                     }
-                    if (!response.isSuccessful) {
-                        _toastMessage.value = response.message()
+                    override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                        _toastMessage.value = t.message
+                        _isLoading.value = false
+                        Log.d(RETROFIT_TAG, t.message.toString())
                     }
-                }
-                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                    _toastMessage.value = t.message
-                    _isLoading.value = false
-                    Log.d(RETROFIT_TAG, t.message.toString())
-                }
 
-            })
+                })
+        }
+
     }
 
 
@@ -214,58 +220,41 @@ class StoryRepository @Inject constructor(
 
 
     fun registerUser(name: String, email: String,password: String) {
-        _isLoading.value = true
-        apiService.registerUser(name, email, password)
-            .enqueue(object : Callback<RegisterResponse> {
-                override fun onResponse(
-                    call: Call<RegisterResponse>,
-                    response: Response<RegisterResponse>
-                ) {
-                    _isLoading.value = false
-                    if (response.isSuccessful) {
-                        Log.d(RETROFIT_TAG, response.body()?.message.toString())
+        wrapEspressoIdlingResource {
+            _isLoading.value = true
+            apiService.registerUser(name, email, password)
+                .enqueue(object : Callback<RegisterResponse> {
+                    override fun onResponse(
+                        call: Call<RegisterResponse>,
+                        response: Response<RegisterResponse>
+                    ) {
+                        _isLoading.value = false
+                        if (response.isSuccessful) {
+                            Log.d(RETROFIT_TAG, response.body()?.message.toString())
+                        }
                     }
-                }
 
-                override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
-                    _isLoading.value = false
-                    Log.d(RETROFIT_TAG, t.message.toString())
-                }
+                    override fun onFailure(call: Call<RegisterResponse>, t: Throwable) {
+                        _isLoading.value = false
+                        Log.d(RETROFIT_TAG, t.message.toString())
+                    }
 
-            })
+                })
+        }
+
     }
 
     fun addStory(token: String, photo: File, description: String){
-        val descriptionText = description.toRequestBody("text/plain".toMediaType())
-        val requestImageFile = photo.asRequestBody("image/jpeg".toMediaTypeOrNull())
-        val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
-            "photo",
-            photo.name,
-            requestImageFile
-        )
-        val service = apiService.uploadImage(token, imageMultipart, descriptionText)
-        service.enqueue(object : Callback<AddResponse> {
-            override fun onResponse(call: Call<AddResponse>, response: Response<AddResponse>) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body()
-                    if (responseBody != null && !responseBody.error) {
-                        _toastMessage.value = responseBody.message
-                    } else {
-                        _toastMessage.value =  response.message()
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<AddResponse>, t: Throwable) {
-                _toastMessage.value = "Gagal instance retrofit"
-            }
-
-        })
-    }
-
-    fun postStoryWithLOcation(token: String, imageMultipart: MultipartBody.Part, description: RequestBody, latitude: Float, longitude: Float) {
-        apiService.uploadImageWithLocation(token, imageMultipart, description, latitude, longitude)
-            .enqueue(object : Callback<AddResponse> {
+        wrapEspressoIdlingResource {
+            val descriptionText = description.toRequestBody("text/plain".toMediaType())
+            val requestImageFile = photo.asRequestBody("image/jpeg".toMediaTypeOrNull())
+            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
+                "photo",
+                photo.name,
+                requestImageFile
+            )
+            val service = apiService.uploadImage(token, imageMultipart, descriptionText)
+            service.enqueue(object : Callback<AddResponse> {
                 override fun onResponse(call: Call<AddResponse>, response: Response<AddResponse>) {
                     if (response.isSuccessful) {
                         val responseBody = response.body()
@@ -282,6 +271,32 @@ class StoryRepository @Inject constructor(
                 }
 
             })
+        }
+
+    }
+
+    fun postStoryWithLOcation(token: String, imageMultipart: MultipartBody.Part, description: RequestBody, latitude: Float, longitude: Float) {
+        wrapEspressoIdlingResource {
+            apiService.uploadImageWithLocation(token, imageMultipart, description, latitude, longitude)
+                .enqueue(object : Callback<AddResponse> {
+                    override fun onResponse(call: Call<AddResponse>, response: Response<AddResponse>) {
+                        if (response.isSuccessful) {
+                            val responseBody = response.body()
+                            if (responseBody != null && !responseBody.error) {
+                                _toastMessage.value = responseBody.message
+                            } else {
+                                _toastMessage.value =  response.message()
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<AddResponse>, t: Throwable) {
+                        _toastMessage.value = "Gagal instance retrofit"
+                    }
+
+                })
+        }
+
     }
 
 }
